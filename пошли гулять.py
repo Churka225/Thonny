ff9ff4e567d08db9e2d212a48ae96cb282f7faa2
ILYA3 <100510119+Churka225@users.noreply.github.com>
2022-10-01 13:28:04 +0300

    Create пошли гулять.py

---
+++ пошли гулять.py
@@ -0,0 +1,31 @@
+    import keyboard
+
+    while True:
+        if keyboard.is_pressed('1') == True:
+            print("привет")
+            break
+    while True:
+        if keyboard.is_pressed('2') == True:
+            print("как дела?")
+            break
+    while True:
+        if keyboard.is_pressed('3') == True:
+            print("пошли гулятб")
+            break
+    while True:
+        if keyboard.is_pressed('4') == True:
+            print("пойдём на площадь")
+            break
+    while True:
+        if keyboard.is_pressed('5') == True:
+            print("зайдём в кифиси")
+            break
+    while True:
+        if keyboard.is_pressed('6') == True:
+            print("потом пойдём на аттракционы")
+            break
+    while True:
+        if keyboard.is_pressed('7') == True:
+            print("короче найдём чем заняться")
+            break
+
