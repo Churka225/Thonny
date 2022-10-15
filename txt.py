72592918cb06dca1d15e516656abe50be488a4d9
ILYA3 <100510119+Churka225@users.noreply.github.com>
2022-10-15 17:55:25 +0700

    Create txt.py

---
+++ txt.py
@@ -0,0 +1,12 @@
+import pyautogui
+import time
+
+#print(pyautogui.position())
+pyautogui.moveTo(481,623)
+pyautogui.click(button = 'right')
+time.sleep(1)
+pyautogui.moveTo(571,561)
+time.sleep(1)
+pyautogui.moveTo(824,716)
+pyautogui.click()
+
