be4e245cc1ed8fa2ba6ff9267543ec4b90b11a53
ILYA3 <100510119+Churka225@users.noreply.github.com>
2022-10-22 13:23:55 +0300

    Create Чат.бот.py

---
+++ Чат.бот.py
@@ -0,0 +1,18 @@
+import pyautogui
+
+comand = input("Введите команду").lower()
+
+while comand !="выход":
+    if comand == "профиль":
+        print("Твой профиль")
+        print("Твой айди:1639490945")
+        print("решено тестов: 5")
+        print("подписка на канал: есть")
+    elif comand == "донаты.":
+        slovo = input("Введите любое слово")
+        print("донаты. " + slovo)
+        
+    comand = input("Введите команду").lower()
+print("Чат-бот закончил")
+            
+    
